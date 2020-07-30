3449d77910d675a03d2987d4e4d3f0e276ac8c88
xLeachimx <michael.huelsman@gmail.com>
2020-07-30 10:58:20 -0400

    SA methods implemented for aso

--- ranking/answer_set_optimization.py
+++ ranking/answer_set_optimization.py
@@ -10,7 +10,7 @@ import sys, os
 sys.path.insert(0, os.path.abspath('..'))
 
 from untility.pref_logic import PrefFormula
-
+from random import randint
 class ASO:
     # Precond:
     #   domain is a valid Domain object.
@@ -20,6 +20,163 @@ class ASO:
     def __init__(self, domain):
         self.domain = domain
         self.ranks = []
+
+    # Precond:
+    #   alt is a valid Alternative object.
+    #
+    # Postcond:
+    #   Returns the satisfaction vector of the alternative assuming DNF formulas.
+    #   Defaults to returning # of ranks +1 for each formula..
+    def eval_DNF(self, alt):
+        result = [[len(self.ranks[i][j])+1 for j in range(len(self.ranks[i]))] for i in range(len(self.ranks))]
+        if not alt.matches(self.domain):
+            return result
+        for i in range(len(self.ranks)):
+            for j in range(len(self.ranks[i])):
+                for k in range(len(self.ranks[i][j])):
+                    if self.ranks[i][j][k].eval_DNF(alt):
+                        result[i][j] = k+1
+                        break
+        return result
+
+    # Precond:
+    #   alt is a valid Alternative object.
+    #
+    # Postcond:
+    #   Returns the satisfaction vector of the alternative assuming CNF formulas.
+    #   Defaults to returning # of ranks +1 for each formula..
+    def eval_CNF(self, alt):
+        result = [[len(self.ranks[i][j])+1 for j in range(len(self.ranks[i]))] for i in range(len(self.ranks))]
+        if not alt.matches(self.domain):
+            return result
+        for i in range(len(self.ranks)):
+            for j in range(len(self.ranks[i])):
+                for k in range(len(self.ranks[i][j])):
+                    if self.ranks[i][j][k].eval_CNF(alt):
+                        result[i][j] = k+1
+                        break
+        return result
+
+    # Precond:
+    #   alt1 is a valid Alternative object.
+    #   alt2 is a valud Alternative object.
+    #   dnf is a boolean which is true when DNF evaluation is used.
+    #
+    # Postcond:
+    #   Returns the relation between alt1 and alt2.
+    def compare(self, alt1, alt2, dnf=True):
+        satVecs = [[],[]]
+        if dnf:
+            satVecs[0] = self.eval_DNF(alt1)
+            satVecs[1] = self.eval_DNF(alt2)
+        else:
+            satVecs[0] = self.eval_CNF(alt1)
+            satVecs[1] = self.eval_CNF(alt2)
+        for i in range(len(self.ranks)):
+            result = self._pareto(satVecs[0][i],satVecs[1][i])
+            if result != Relation.equal():
+                return result
+        return Relation.equal()
+
+    # Precond:
+    #   sVec1 is a list of integers.
+    #   sVec2 is a list of integers.
+    #
+    # Postcond:
+    #   Returns the relation between the two satisfaction vectors based on
+    #   Pareto dominance.
+    def _pareto(self, sVec1, sVec2):
+        if len(sVec1) != len(sVec2):
+            return Relation.incomparable()
+        result = Relation.equal()
+        for i in range(sVec1):
+            if result == Relation.equal():
+                if sVec1[i] < sVec2[i]:
+                    result = Relation.strict_preference()
+                elif sVec2[i] < sVec1[i]:
+                    result = Relation.strict_dispreference()
+            elif result == Relation.strict_preference():
+                if sVec2[i] < sVec1[i]:
+                    return Relation.incomparable()
+            elif result == Relation.strict_dispreference():
+                if sVec1[i] < sVec2[i]:
+                    return Relation.incomparable()
+        return result
+
+
+    # TODO: Implement these properly.
+    # Precond:
+    #   None.
+    #
+    # Postcond:
+    #   Returns a random RankingPrefFormula object which is a neighbor of this
+    #   RankingPrefFormula object.
+    def random_neighbor(self):
+        result = self._copy()
+        # Select the formula to change
+        choice = randint(0,self.formula_length()-1)
+        index = self.formula_index(choice)
+        rank = index[0]
+        rule = index[1]
+        formula = index[2]
+        result.ranks[rank][rule][formula] = result.ranks[rank][rule][formula].random_neighbor()
+        return result
+
+    # Precond:
+    #   None.
+    #
+    # Postcond:
+    #   Returns the number of formulas in the ASO.
+    def formula_length(self):
+        total = 0
+        for i in range(len(self.ranks)):
+            for j in range(len(self.ranks[i])):
+                total += len(self.ranks[i][j])
+        return total
+
+    # Precond:
+    #   n is an integer indicating which formula index to get.
+    #
+    # Postcond:
+    #   Returns the index tuple (rank,rule,formula) of the formula which is the
+    #   nth formula for the ASO.
+    def formula_index(self, n):
+        for i in range(len(self.ranks)):
+            for j in range(len(self.ranks[i])):
+                if n >= len(self.ranks[i][j]):
+                    n -= len(self.ranks[i][j])
+                else:
+                    return (i,j,n)
+        return (0,0,0)
+
+    # Precond:
+    #   None.
+    #
+    # Postcond:
+    #   Iterates through all neighbors of this RankingPrefFormula object.
+    def neighbors(self):
+        for i in range(len(self.ranks)):
+            for j in range(len(self.ranks[i])):
+                for k in range(len(self.ranks[k])):
+                    result = self._copy()
+                    for neighbor in self.ranks[i][j][k].neighbors():
+                        result.ranks[i][j][k] = neighbor
+                    yield result
+
+    # Precond:
+    #   None.
+    #
+    # Postcond:
+    #   Returns a completely separate copy of the ASO.
+    def _copy(self):
+        result = ASO(self.domain)
+        result.ranks = [[[] for j in range(len(self.ranks[i])] for i in range(len(self.ranks))]
+        for i in range(len(self.ranks)):
+            for j in range(len(self.ranks[i])):
+                for k in range(len(self.ranks[i][j])):
+                    result.ranks[i][j].append(self.ranks[i][j][k])
+        return result
+
 
 
     # Precond:
